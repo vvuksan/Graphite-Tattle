d82c3473eb2f3feaecfcb47b0a345532386a7b05
vuksan <vuksan@mygpslocation.com>
2011-11-05 10:51:23 -0700

    Make assets path relative

--- inc/init.php
+++ inc/init.php
@@ -19,12 +19,12 @@ if (!is_dir(JS_CACHE) || !is_writable(JS_CACHE)){
   $tmpl->enableMinification('development', JS_CACHE, TATTLE_ROOT);
 }
 $tmpl->add('css','/bootstrap/bootstrap.min.css'); 
-$tmpl->add('css','/assets/css/jquery-ui.css');
+$tmpl->add('css','assets/css/jquery-ui.css');
 
-$tmpl->add('js','/assets/js/jquery.min.js'); 
-$tmpl->add('js','/assets/js/jquery-ui.min.js'); 
-$tmpl->add('js','/assets/js/jquery.collapsible.js'); 
-$tmpl->add('js','/assets/js/jquery.graphite.js');
+$tmpl->add('js','assets/js/jquery.min.js'); 
+$tmpl->add('js','assets/js/jquery-ui.min.js'); 
+$tmpl->add('js','assets/js/jquery.collapsible.js'); 
+$tmpl->add('js','assets/js/jquery.graphite.js');
 
 $tmpl->add('js','/bootstrap/js/bootstrap-modal.js');
 $tmpl->add('js','/bootstrap/js/bootstrap-twipsy.js');
